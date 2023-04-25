8cb4580c0924b3a9fa57a8be19d6a4b72dfd7f3e
Balsi <balsi@balsi.nde.tw>
2023-04-25 20:42:27 +0800

    新增imageview

--- login/app/src/main/java/com/example/myapplication/MainActivity.java
+++ login/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.myapplication;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,7 +26,25 @@ public class MainActivity extends AppCompatActivity {
       Intent intent = new Intent(MainActivity.this, signingActivity.class);
       startActivity(intent);
     }
-
+   ImageView[] n=new ImageView[5];
+    LinearLayout t=findViewById(R.id.l1);
+    for(int i=0;i<5;i++){
+      ImageView ii=new ImageView(this);
+      ii.setImageResource(R.drawable.burger);
+      ii.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT));
+      n[i]=ii;
+      t.addView(ii);
+    }
+    for(int i=0;i<5;i++)
+    {
+      n[i].setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          Intent nn=new Intent(MainActivity.this,signingActivity.class);
+          startActivity(nn);
+        }
+      });
+    }
     /*btnclear = findViewById(R.id.btn_clear);
     View.OnClickListener listener = new View.OnClickListener() {
       @Override
