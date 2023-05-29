a0ff142c5fdd157f9919407ae2124f01386261bb
Liu Deng Ying <aa0588175@gmail.com>
2023-05-29 14:11:51 +0800

    內用外帶按鈕點選後提示訊息

--- login/app/src/main/java/com/example/myapplication/pay_page.java
+++ login/app/src/main/java/com/example/myapplication/pay_page.java
@@ -1,8 +1,13 @@
 package com.example.myapplication;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class pay_page extends AppCompatActivity {
@@ -22,5 +27,50 @@ public class pay_page extends AppCompatActivity {
     textView=findViewById(R.id.et_pay_meal_list);
 
     textView.setText(name+" "+price+"\n");
+  }
+  public void onClick(View view) {
+    String num =tvPhoneNum.getText().toString();
+
+    switch (view.getId()){
+      case R.id.bt_in_use:
+        AlertDialog.Builder dialog = new AlertDialog.Builder( MainActivity.this );
+        dialog.setTitle("確認視窗");
+        dialog.setIcon(R.mipmap.ic_launcher);
+        dialog.setMessage("請至櫃台結帳");
+        dialog.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialogInterface, int i) {
+            finish();
+          }
+        });
+        dialog.setNegativeButton( "取消", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialogInterface, int i) {
+
+          }
+        });
+        dialog.show();
+        break;
+      case R.id.btn_close:
+        AlertDialog.Builder dialog = new AlertDialog.Builder( MainActivity.this );
+        dialog.setTitle("確認視窗");
+        dialog.setIcon(R.mipmap.ic_launcher);
+        dialog.setMessage("請至櫃台結帳");
+        dialog.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialogInterface, int i) {
+            finish();
+          }
+        });
+        dialog.setNegativeButton( "取消", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialogInterface, int i) {
+
+          }
+        });
+        dialog.show();
+        break;
+    }
+  }
   }
 }
